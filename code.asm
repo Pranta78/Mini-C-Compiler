b0623c6f8cbaf053a79be5e977b0df1fea78f87d
Pranta78 <1705078@ugrad.cse.buet.ac.bd>
2021-05-10 18:25:32 +0600

    CP before removing code for saving local vars before calling non-recursive function

--- code.asm
+++ code.asm
@@ -1,266 +1,184 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-	var_1_1_a	DW	?
-	var_1_1_b	DW	?
-	var_1_1_c	DW	?
-	var_1_1_k	DW	?
+	var_1_1_m	DW	?
 	var_1_1_n	DW	?
-	var_1_2_a	DW	?
-	var_1_2_b	DW	?
-	var_1_2_c	DW	?
-	var_1_2_d	DW	?
-	var_1_2_e	DW	?
-	var_1_2_f	DW	?
+	var_1_2_m	DW	?
+	var_1_2_n	DW	?
+	var_1_3_a	DW	?
+	var_1_3_b	DW	?
+	var_1_3_c	DW	?
+	var_1_3_d	DW	?
 	t0	DW	?
-	t1	DW	?
 .CODE
 
-_binom PROC
+_f1 PROC
 	PUSH BP
 	MOV BP, SP
 
+	;m=n-1
 	MOV AX, WORD PTR [BP+4]
-	CMP AX, WORD PTR [BP+6]
+	SUB AX, 1
+	MOV t0, AX
+	MOV AX, t0
+	MOV var_1_1_m, AX
+
+	MOV AX, WORD PTR [BP+4]
+	CMP AX, 0
 	JNE L0
 	MOV t0, 1
 	JMP L1
 L0:
 	AND t0, 0
 L1:
-	MOV AX, WORD PTR [BP+4]
-	CMP AX, 0
-	JNE L2
-	MOV t1, 1
-	JMP L3
-L2:
-	AND t1, 0
-L3:
 	CMP t0, 0
-	JNE L4
-	CMP t1, 0
-	JNE L4
-	AND t0, 0
-	JMP L5
-L4:
-	MOV t0, 1
-L5:
-	CMP t0, 0
-	JE L6
+	JE L2
 	MOV DX, 1
 	POP BP
-	RET 4
-L6:
-	;a=binom(n-1,k)
+	RET 2
+L2:
 	;saving local variables in the stack before function call
-	PUSH var_1_1_a
+	PUSH var_1_1_m
 ;arguments:
-	MOV AX, WORD PTR [BP+6]
-	SUB AX, 1
-	MOV t0, AX
-	PUSH t0
-	PUSH WORD PTR [BP+4]
-	CALL _binom
+	PUSH var_1_1_m
+	CALL _f2
 	;saving the return value in DX in a temporary variable
 	MOV t0, DX
 	;restoring local variables from the stack after function call
-	POP var_1_1_a
+	POP var_1_1_m
 	MOV AX, t0
-	MOV var_1_1_a, AX
-
-	;b=binom(n-1,k-1)
-	;saving local variables in the stack before function call
-	PUSH var_1_1_a
-	PUSH var_1_1_b
-;arguments:
-	MOV AX, WORD PTR [BP+6]
-	SUB AX, 1
+	IMUL WORD PTR [BP+4]
 	MOV t0, AX
-	PUSH t0
+	MOV DX, t0
+	POP BP
+	RET 2
+_f1 ENDP
+
+_f2 PROC
+	PUSH BP
+	MOV BP, SP
+
+	;m=n-1
 	MOV AX, WORD PTR [BP+4]
 	SUB AX, 1
 	MOV t0, AX
-	PUSH t0
-	CALL _binom
+	MOV AX, t0
+	MOV var_1_2_m, AX
+
+	MOV AX, WORD PTR [BP+4]
+	CMP AX, 0
+	JNE L3
+	MOV t0, 1
+	JMP L4
+L3:
+	AND t0, 0
+L4:
+	CMP t0, 0
+	JE L5
+	MOV DX, 1
+	POP BP
+	RET 2
+L5:
+	;saving local variables in the stack before function call
+	PUSH var_1_2_m
+;arguments:
+	PUSH var_1_2_m
+	CALL _f1
 	;saving the return value in DX in a temporary variable
 	MOV t0, DX
 	;restoring local variables from the stack after function call
-	POP var_1_1_b
-	POP var_1_1_a
+	POP var_1_2_m
 	MOV AX, t0
-	MOV var_1_1_b, AX
-
-	;c=a+b
-	MOV AX, var_1_1_a
-	ADD AX, var_1_1_b
+	IMUL WORD PTR [BP+4]
 	MOV t0, AX
-	MOV AX, t0
-	MOV var_1_1_c, AX
-
-	MOV DX, var_1_1_c
+	MOV DX, t0
 	POP BP
-	RET 4
-_binom ENDP
+	RET 2
+_f2 ENDP
 
 MAIN PROC
 	MOV AX, @DATA
 	MOV DS, AX
 
-	;a=binom(3,0)
+	;a=f1(4)
 	;saving local variables in the stack before function call
-	PUSH var_1_2_a
-	PUSH var_1_2_b
-	PUSH var_1_2_c
-	PUSH var_1_2_d
-	PUSH var_1_2_e
-	PUSH var_1_2_f
-;arguments:
-	MOV AX, 3
-	PUSH AX
-	MOV AX, 0
-	PUSH AX
-	CALL _binom
-	;saving the return value in DX in a temporary variable
-	MOV t0, DX
-	;restoring local variables from the stack after function call
-	POP var_1_2_f
-	POP var_1_2_e
-	POP var_1_2_d
-	POP var_1_2_c
-	POP var_1_2_b
-	POP var_1_2_a
-	MOV AX, t0
-	MOV var_1_2_a, AX
-
-	;b=binom(3,1)
-	;saving local variables in the stack before function call
-	PUSH var_1_2_a
-	PUSH var_1_2_b
-	PUSH var_1_2_c
-	PUSH var_1_2_d
-	PUSH var_1_2_e
-	PUSH var_1_2_f
-;arguments:
-	MOV AX, 3
-	PUSH AX
-	MOV AX, 1
-	PUSH AX
-	CALL _binom
-	;saving the return value in DX in a temporary variable
-	MOV t0, DX
-	;restoring local variables from the stack after function call
-	POP var_1_2_f
-	POP var_1_2_e
-	POP var_1_2_d
-	POP var_1_2_c
-	POP var_1_2_b
-	POP var_1_2_a
-	MOV AX, t0
-	MOV var_1_2_b, AX
-
-	;c=binom(3,2)
-	;saving local variables in the stack before function call
-	PUSH var_1_2_a
-	PUSH var_1_2_b
-	PUSH var_1_2_c
-	PUSH var_1_2_d
-	PUSH var_1_2_e
-	PUSH var_1_2_f
-;arguments:
-	MOV AX, 3
-	PUSH AX
-	MOV AX, 2
-	PUSH AX
-	CALL _binom
-	;saving the return value in DX in a temporary variable
-	MOV t0, DX
-	;restoring local variables from the stack after function call
-	POP var_1_2_f
-	POP var_1_2_e
-	POP var_1_2_d
-	POP var_1_2_c
-	POP var_1_2_b
-	POP var_1_2_a
-	MOV AX, t0
-	MOV var_1_2_c, AX
-
-	;d=binom(3,3)
-	;saving local variables in the stack before function call
-	PUSH var_1_2_a
-	PUSH var_1_2_b
-	PUSH var_1_2_c
-	PUSH var_1_2_d
-	PUSH var_1_2_e
-	PUSH var_1_2_f
-;arguments:
-	MOV AX, 3
-	PUSH AX
-	MOV AX, 3
-	PUSH AX
-	CALL _binom
-	;saving the return value in DX in a temporary variable
-	MOV t0, DX
-	;restoring local variables from the stack after function call
-	POP var_1_2_f
-	POP var_1_2_e
-	POP var_1_2_d
-	POP var_1_2_c
-	POP var_1_2_b
-	POP var_1_2_a
-	MOV AX, t0
-	MOV var_1_2_d, AX
-
-	;e=binom(4,2)
-	;saving local variables in the stack before function call
-	PUSH var_1_2_a
-	PUSH var_1_2_b
-	PUSH var_1_2_c
-	PUSH var_1_2_d
-	PUSH var_1_2_e
-	PUSH var_1_2_f
+	PUSH var_1_3_a
+	PUSH var_1_3_b
+	PUSH var_1_3_c
+	PUSH var_1_3_d
 ;arguments:
 	MOV AX, 4
 	PUSH AX
-	MOV AX, 2
-	PUSH AX
-	CALL _binom
+	CALL _f1
 	;saving the return value in DX in a temporary variable
 	MOV t0, DX
 	;restoring local variables from the stack after function call
-	POP var_1_2_f
-	POP var_1_2_e
-	POP var_1_2_d
-	POP var_1_2_c
-	POP var_1_2_b
-	POP var_1_2_a
+	POP var_1_3_d
+	POP var_1_3_c
+	POP var_1_3_b
+	POP var_1_3_a
 	MOV AX, t0
-	MOV var_1_2_e, AX
+	MOV var_1_3_a, AX
 
-	;f=binom(4,3)
+	;b=f2(5)
 	;saving local variables in the stack before function call
-	PUSH var_1_2_a
-	PUSH var_1_2_b
-	PUSH var_1_2_c
-	PUSH var_1_2_d
-	PUSH var_1_2_e
-	PUSH var_1_2_f
+	PUSH var_1_3_a
+	PUSH var_1_3_b
+	PUSH var_1_3_c
+	PUSH var_1_3_d
 ;arguments:
-	MOV AX, 4
+	MOV AX, 5
 	PUSH AX
-	MOV AX, 3
-	PUSH AX
-	CALL _binom
+	CALL _f2
 	;saving the return value in DX in a temporary variable
 	MOV t0, DX
 	;restoring local variables from the stack after function call
-	POP var_1_2_f
-	POP var_1_2_e
-	POP var_1_2_d
-	POP var_1_2_c
-	POP var_1_2_b
-	POP var_1_2_a
+	POP var_1_3_d
+	POP var_1_3_c
+	POP var_1_3_b
+	POP var_1_3_a
 	MOV AX, t0
-	MOV var_1_2_f, AX
+	MOV var_1_3_b, AX
+
+	;c=f1(6)
+	;saving local variables in the stack before function call
+	PUSH var_1_3_a
+	PUSH var_1_3_b
+	PUSH var_1_3_c
+	PUSH var_1_3_d
+;arguments:
+	MOV AX, 6
+	PUSH AX
+	CALL _f1
+	;saving the return value in DX in a temporary variable
+	MOV t0, DX
+	;restoring local variables from the stack after function call
+	POP var_1_3_d
+	POP var_1_3_c
+	POP var_1_3_b
+	POP var_1_3_a
+	MOV AX, t0
+	MOV var_1_3_c, AX
+
+	;d=f2(7)
+	;saving local variables in the stack before function call
+	PUSH var_1_3_a
+	PUSH var_1_3_b
+	PUSH var_1_3_c
+	PUSH var_1_3_d
+;arguments:
+	MOV AX, 7
+	PUSH AX
+	CALL _f2
+	;saving the return value in DX in a temporary variable
+	MOV t0, DX
+	;restoring local variables from the stack after function call
+	POP var_1_3_d
+	POP var_1_3_c
+	POP var_1_3_b
+	POP var_1_3_a
+	MOV AX, t0
+	MOV var_1_3_d, AX
 
 
 	;exit program

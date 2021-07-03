c9e9b41af22865e1397969cba141fe10903770f0
Pranta78 <1705078@ugrad.cse.buet.ac.bd>
2021-07-03 20:47:42 +0600

    CP Before fixing bug: declaring function names in DATA segment

--- code.asm
+++ code.asm
@@ -1,12 +1,12 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-	var_1_1_a	DW	?
-	var_1_2_x	DW	?
-	var_1_2_a	DW	?
-	var_1_2_b	DW	?
-	var_1_3_a	DW	?
-	var_1_3_b	DW	?
+	var_1_1_x	DW	?
+	var_1_2_y	DW	?
+	var_1_3_z	DW	?
+	var_1_4_a	DW	?
+	var_1_f	DW	?
+	var_1_g	DW	?
 	t0	DW	?
 .CODE
 
@@ -14,15 +14,12 @@ _f PROC
 	PUSH BP
 	MOV BP, SP
 
-	MOV AX, 2
+	MOV AX, WORD PTR [BP+4]
 	IMUL WORD PTR [BP+4]
 	MOV t0, AX
 	MOV DX, t0
 	POP BP
 	RET 2
-	;a=9
-	MOV WORD PTR [BP+4], 9
-
 
 	POP BP
 	RET 2
@@ -32,53 +29,60 @@ _g PROC
 	PUSH BP
 	MOV BP, SP
 
-	;x=f(a)+a+b
-;arguments:
-	PUSH WORD PTR [BP+6]
-	CALL _f
-	;saving the return value in DX in a temporary variable
-	MOV t0, DX
-	MOV AX, t0
-	ADD AX, WORD PTR [BP+6]
-	MOV t0, AX
-	MOV AX, t0
+	MOV AX, WORD PTR [BP+4]
 	ADD AX, WORD PTR [BP+4]
 	MOV t0, AX
-	MOV AX, t0
-	MOV var_1_2_x, AX
-
-	MOV DX, var_1_2_x
+	MOV DX, t0
 	POP BP
-	RET 4
+	RET 2
 
 	POP BP
-	RET 4
+	RET 2
 _g ENDP
+
+_h PROC
+	PUSH BP
+	MOV BP, SP
+
+	MOV AX, 3
+	IMUL WORD PTR [BP+4]
+	MOV t0, AX
+	MOV DX, t0
+	POP BP
+	RET 2
+
+	POP BP
+	RET 2
+_h ENDP
 
 MAIN PROC
 	MOV AX, @DATA
 	MOV DS, AX
 
-	;a=1
-	MOV var_1_3_a, 1
+	;a=3
+	MOV var_1_4_a, 3
 
-	;b=2
-	MOV var_1_3_b, 2
-
-	;a=g(a,b)
+	;a=h(g(f(a)))
 ;arguments:
-	PUSH var_1_3_a
-	PUSH var_1_3_b
+;arguments:
+;arguments:
+	PUSH var_1_4_a
+	CALL _f
+	;saving the return value in DX in a temporary variable
+	MOV t0, DX
+	PUSH t0
 	CALL _g
 	;saving the return value in DX in a temporary variable
 	MOV t0, DX
+	PUSH t0
+	CALL _h
+	;saving the return value in DX in a temporary variable
+	MOV t0, DX
 	MOV AX, t0
-	MOV var_1_3_a, AX
+	MOV var_1_4_a, AX
 
-	PUSH var_1_3_a
+	PUSH var_1_4_a
 	CALL PRINTLN
-	MOV AH, 4CH
-	INT 21H
 
 	;exit program
 	MOV AH, 4CH
